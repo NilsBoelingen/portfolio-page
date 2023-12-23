d90d8edbb70e34b6064f9b63e563a341a6b3a01e
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2023-12-23 16:26:48 +0100

    add hero + about me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AboveDefaultComponent } from './above-default/above-default.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, AboveDefaultComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, AboveDefaultComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
