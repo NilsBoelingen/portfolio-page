5e13938f051889de340f4139de6f4cff93fe0717
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2023-12-27 14:23:21 +0100

    add some components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { AboveDefaultComponent } from './above-default/above-default.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, AboveDefaultComponent, AboutMeComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, AboveDefaultComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
