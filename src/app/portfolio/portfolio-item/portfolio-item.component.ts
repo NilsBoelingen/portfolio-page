a81bc09d031b7d998f7f3ef52428926781a28584
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2023-12-30 23:03:18 +0100

    add portfolio item component

---
+++ src/app/portfolio/portfolio-item/portfolio-item.component.ts
@@ -0,0 +1,24 @@
+import { NgFor, NgForOf } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { PortfolioComponent } from '../portfolio.component';
+
+@Component({
+  selector: 'app-portfolio-item',
+  standalone: true,
+  imports: [NgFor, NgForOf, PortfolioComponent],
+  templateUrl: './portfolio-item.component.html',
+  styleUrl: './portfolio-item.component.scss'
+})
+export class PortfolioItemComponent implements OnInit {
+
+  @Input() index!:number;
+  @Input() projects: any[] = [];
+
+  constructor() {
+    
+  }
+
+  ngOnInit(): void {
+
+  }
+}
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
+import { PortfolioItemComponent } from './portfolio-item/portfolio-item.component';
+import { CommonModule, NgFor, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [PortfolioItemComponent, NgFor, NgForOf, CommonModule, PortfolioItemComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
+
+  public projects = [
+    {
+      titel: "Join",
+      Languages: "HTML | CSS | JavaScript",
+      describtion: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+      image: "assets/img/portfolio/join.png"
+    },
+    {
+      titel: "Skarkie",
+      Languages: "HTML | CSS | JavaScript",
+      describtion: "A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.",
+      image: "assets/img/portfolio/sharkie.png"
+    },
+    {
+      titel: "Simple CRM",
+      Languages: "Angular | Firebase",
+      describtion: "A very Simple Customer Relationship Management system working with CRUD functionality.",
+      image: "assets/img/portfolio/simple_crm.png"
+    },
+    {
+      titel: "Pokédex",
+      Languages: "HTML | CSS | JavaScript | Api",
+      describtion: "Based on the PokéAPI a simple library that provides and catalogues pokemon information.",
+      image: "assets/img/portfolio/pokedex.png"
+    },
+    {
+      titel: "DA Bubble",
+      Languages: "HTML | CSS | JavaScript",
+      describtion: "A simple messanger inspired by Slack. Get and Post Messages to your Team or your friends.",
+      image: "assets/img/portfolio/da_bubble.png"
+    },
+    {
+      titel: "Ring of Fire",
+      Languages: "Angular | Firebase | TypeScript",
+      describtion: "A simple Card game based on the Popular drink game.",
+      image: "assets/img/portfolio/ring_of_fire.png"
+    },
+  ]
 
   scrollbarWidth: number = 17;
   headlineGap: number = 42;
