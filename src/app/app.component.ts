f29991349dfbfb9645c3dd7eb755d5cc8c139f04
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2024-01-02 22:10:36 +0100

    update strukture and add imprint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { AboveDefaultComponent } from './above-default/above-default.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { SkillsComponent } from './skills/skills.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, AboveDefaultComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ContactComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
