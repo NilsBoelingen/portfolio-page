33ce616028537e61146fbd10a7f93519d76a2cb9
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2024-01-09 12:42:50 +0100

    add menu for mobile

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -10,11 +11,21 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  menuButtonImg = ['assets/img/header/burger.png', 'assets/img/header/Transition.png', 'assets/img/header/close_medium.png', 'assets/img/header/CLOSE_FINAL.png']
+
+  @ViewChild('menuButton') menuButton!: ElementRef;
+  @ViewChild('menuDialog') menuDialog!: ElementRef;
+
+  menuOpen: boolean = false;
+  currentImage: number = 0;
+
   public aboutMe: boolean = false;
   public skills: boolean = false;
   public portfolio: boolean = false;
 
   public selectedItem: string | null = null;
+
+  constructor(private renderer: Renderer2) { }
 
   public selectItem(item: string): void {
     this.portfolio = false;
@@ -29,5 +40,43 @@ export class HeaderComponent {
     this.portfolio = false;
     this.skills = false;
     this.selectedItem = null;
+  }
+
+  openMenu() {
+    if (!this.menuOpen) {
+      this.menuOpen = true;      
+      this.startImgAnimationOpen();
+      this.renderer.removeClass(this.menuDialog.nativeElement, 'hide-menu');
+      this.renderer.addClass(this.menuDialog.nativeElement, 'show-menu');
+    }else if (this.menuOpen) {
+      this.menuOpen = false;
+      this.startImgAnimationClose();
+      this.renderer.removeClass(this.menuDialog.nativeElement, 'show-menu');
+      this.renderer.addClass(this.menuDialog.nativeElement, 'hide-menu');
+    }
+  }
+
+  startImgAnimationOpen() {
+    let interval = setInterval(() => {
+      if (this.currentImage < this.menuButtonImg.length - 1) {
+        this.currentImage = (this.currentImage + 1) % this.menuButtonImg.length;
+        const newImagePath = this.menuButtonImg[this.currentImage];
+        this.renderer.setAttribute(this.menuButton.nativeElement, 'src', newImagePath);
+      } else {
+        clearInterval(interval);
+      }
+    }, 80);
+  }
+
+  startImgAnimationClose() {
+    let interval = setInterval(() => {
+      if (this.currentImage > 0) {
+        this.currentImage = (this.currentImage - 1) % this.menuButtonImg.length;;
+        const newImagePath = this.menuButtonImg[this.currentImage];
+        this.renderer.setAttribute(this.menuButton.nativeElement, 'src', newImagePath);
+      } else {
+        clearInterval(interval);
+      }
+    }, 80);
   }
 }
