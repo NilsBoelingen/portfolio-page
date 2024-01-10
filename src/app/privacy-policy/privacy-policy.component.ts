d0392089288fe933087ece2afd3180bcd83c64de
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2024-01-10 21:11:29 +0100

    bugfix routes and design

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
