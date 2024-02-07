46d6199feceac1e2c0444225d26ac90f178e6c07
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2024-02-07 10:25:41 +0100

    add new content to portfolio

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -5,62 +5,55 @@ import { CommonModule, NgFor, NgForOf } from '@angular/common';
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [PortfolioItemComponent, NgFor, NgForOf, CommonModule, PortfolioItemComponent],
+  imports: [
+    PortfolioItemComponent,
+    NgFor,
+    NgForOf,
+    CommonModule,
+    PortfolioItemComponent,
+  ],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
-
   public projects = [
     {
-      titel: "Join",
-      Languages: "HTML | CSS | JavaScript",
-      describtion: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
-      image: "assets/img/portfolio/join.png",
-      test: "https://join.nils-boelingen.de/index.html",
-      github: "https://github.com/NilsBoelingen/Join",
+      titel: 'Join',
+      Languages: 'HTML | CSS | JavaScript',
+      describtion:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      image: 'assets/img/portfolio/join.png',
+      test: 'https://join.nils-boelingen.de/index.html',
+      github: 'https://github.com/NilsBoelingen/Join',
     },
     {
-      titel: "Sharkie",
-      Languages: "HTML | CSS | JavaScript",
-      describtion: "A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.",
-      image: "assets/img/portfolio/sharkie.png",
-      test: "https://sharkie.nils-boelingen.de/index.html",
-      github: "https://github.com/NilsBoelingen/Sharkie",
+      titel: 'Sharkie',
+      Languages: 'HTML | CSS | JavaScript',
+      describtion:
+        'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
+      image: 'assets/img/portfolio/sharkie.png',
+      test: 'https://sharkie.nils-boelingen.de/index.html',
+      github: 'https://github.com/NilsBoelingen/Sharkie',
     },
-    // {
-    //   titel: "Simple CRM",
-    //   Languages: "Angular | Firebase",
-    //   describtion: "A very Simple Customer Relationship Management system working with CRUD functionality.",
-    //   image: "assets/img/portfolio/simple_crm.png",
-    // test: "",
-    // github: "",
-    // },
     {
-      titel: "Pokédex",
-      Languages: "HTML | CSS | JavaScript | Api",
-      describtion: "Based on the PokéAPI a simple library that provides and catalogues pokemon information.",
-      image: "assets/img/portfolio/pokedex.png",
-      test: "https://pokedex.nils-boelingen.de/index.html",
-      github: "https://github.com/NilsBoelingen/Pokedex",
+      titel: 'Pokédex',
+      Languages: 'HTML | CSS | JavaScript | Api',
+      describtion:
+        'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
+      image: 'assets/img/portfolio/pokedex.png',
+      test: 'https://pokedex.nils-boelingen.de/index.html',
+      github: 'https://github.com/NilsBoelingen/Pokedex',
     },
-    // {
-    //   titel: "DA Bubble",
-    //   Languages: "HTML | CSS | JavaScript",
-    //   describtion: "A simple messanger inspired by Slack. Get and Post Messages to your Team or your friends.",
-    //   image: "assets/img/portfolio/da_bubble.png",
-    // test: "",
-    // github: "",
-    // },
     {
-      titel: "Ring of Fire",
-      Languages: "Angular | Firebase | TypeScript",
-      describtion: "A simple Card game based on the Popular drinking game.",
-      image: "assets/img/portfolio/ring_of_fire.png",
-      test: "https://ringoffire.nils-boelingen.de/index.html",
-      github: "https://github.com/NilsBoelingen/ringoffire",
+      titel: 'Simple CRM',
+      Languages: 'Angular | Firebase',
+      describtion:
+        'A very Simple Customer Relationship Management system working with CRUD functionality.',
+      image: 'assets/img/portfolio/simple_crm.png',
+      test: 'https://simplecrm.nils-boelingen.de/',
+      github: 'https://github.com/NilsBoelingen/simple-crm',
     },
-  ]
+  ];
 
   scrollbarWidth: number = 17;
   headlineGap: number = 42;
@@ -71,7 +64,11 @@ export class PortfolioComponent {
 
   changeLineWidth() {
     let headline: any = document.getElementById('headline');
-    let lineWidth = (window.innerWidth / 2) - (headline?.clientWidth / 2) - this.scrollbarWidth - this.headlineGap;
+    let lineWidth =
+      window.innerWidth / 2 -
+      headline?.clientWidth / 2 -
+      this.scrollbarWidth -
+      this.headlineGap;
     let rightLine: any = document.getElementById('rightLine');
     rightLine.style.width = `${lineWidth}px`;
   }
